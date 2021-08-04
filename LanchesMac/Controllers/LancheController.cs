ddb2ee74343452eb3b3226ab771103d088758bd1
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-04 00:18:25 -0300

    Criada a view e LancheController ja está exibindo os lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository )
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categorias";
+            var lanches = _lancheRepository.Lanches;
+
+            return View(lanches);
+        }
+    }
+}
